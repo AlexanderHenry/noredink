5736d227b42a872247aa6d11a8e6d014736b2dda
Alex Henry <alexanderhenry@Alexs-MacBook-Pro.local>
2017-04-04 18:45:42 -0500

    logic for negative numbers

--- quiz.rb
+++ quiz.rb
@@ -1,11 +1,31 @@
 require 'csv'
 def quiz
   csv = CSV.read('questions.csv')
+  x = 0
+  #CSV.foreach('questions.csv') do |row|
+  #  strand_id = csv[0][x]
+  #  puts "strand id = #{x}"
+  #  x++
+  #end
+  #if @question_amount < 1
+  #  "Please enter a positive number"
+  #  questions
+  #elsif @question_amount.is_a?(String) == true
+  #  "Please enter a number"
+  #  questions
+  #else
+  #  puts @question_amount
+  #end
 
+  #puts strand_id
+  question_amount = 1
   puts "How many questions would you like to answer?"
-  question_amount = gets.chomp
+  question_amount = gets.chomp.to_i
 
-  puts question_amount
+  while question_amount < 1
+    puts "Number must be positive"
+    question_amount = gets.chomp.to_i
+  end
 end
 
 quiz
