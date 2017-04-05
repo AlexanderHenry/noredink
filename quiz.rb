986ec6d161b63ee1ceef8b52140e0a621f0c6acf
Alex Henry <alexanderhenry@Alexs-MacBook-Pro.local>
2017-04-04 19:07:45 -0500

    get question_id's and display them

--- quiz.rb
+++ quiz.rb
@@ -1,14 +1,7 @@
 require 'csv'
 def quiz
   csv = CSV.read('questions.csv')
-  x = 0
-  #CSV.foreach('questions.csv') do |row|
-  #  strand_id = csv[0][x]
-  #  puts "strand id = #{x}"
-  #  x++
-  #end
 
-  #puts strand_id
   puts "How many questions would you like to answer?"
   question_amount = gets.chomp.to_i
 
@@ -19,6 +12,10 @@ def quiz
   if question_amount.is_a?(String) == true
     puts "Must be a number"
     question_amount = gets.chomp.to_i
+  end
+
+  CSV.foreach('questions.csv', :headers => true) do |question_id|
+    puts "Question ID##{question_id[4]}"
   end
 end
 
