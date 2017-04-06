d4e6c8af03ec1abcd5940a605672087c2a1f72a7
Alex Henry <alexanderhenry@alexs-mbp.dev-lab.ec.jamfsw.corp>
2017-04-06 15:48:27 -0500

    Number of questions dependent on number given by user

--- quiz.rb
+++ quiz.rb
@@ -14,9 +14,28 @@ def quiz
     question_amount = gets.chomp.to_i
   end
 
+  question_hash = Hash.new {|hash, key| hash[key] = [] }
+  response_array = Array.new
+
   CSV.foreach('questions.csv', :headers => true) do |question_id|
-    puts "Question ID##{question_id[4]}"
+    rand_questions = question_hash[question_id["question_id"]] << question_id["question_id"]
+    response_array.push(rand_questions)
+  end
+
+  if question_amount % 12 == 0
+    multi = question_amount / 12
+    while multi > 0
+      puts response_array.sample(question_amount)
+      multi = multi - 1
+    end
+  else question_amount % 12 != 0
+    multi = ((question_amount - (question_amount%12)) / 12)
+    remainder = (question_amount % 12)
+    while multi > 0
+      puts response_array.sample(12)
+      multi = multi - 1
+    end
+    puts response_array.sample(remainder)
   end
 end
-
 quiz
